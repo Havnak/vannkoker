b57eed5690b9521c3e050d49ff49b76f85c7f4f1
havnak <havardnakling@gmail.com>
2024-09-05 23:10:29 +0200

    feat: test setup for pid and kettle with thermometer

--- rpi/kettle.py
+++ rpi/kettle.py
@@ -11,7 +11,7 @@ class Kettle:
 
     def __init__(self, physical_output_pin) -> None:
 
-        self.temperature = 0
+        self.temperature = self.read_thermometer()
         self.goal_temperature = 0
         self.thermometer_history = []
         self.power_level = 0
@@ -19,7 +19,7 @@ class Kettle:
         self.time_0_s = time.ticks_ms()*1000
         
         self.physical_thermometer = ADC(28)
-        self.physical_output_pin = Pin(physical_output_pin) # TODO
+        self.physical_output_pin = Pin(physical_output_pin, Pin.OUT) # TODO
         
     def set_goal(self, goal_temperature) -> None:
         '''
@@ -30,14 +30,21 @@ class Kettle:
         '''
         self.goal_temperature = goal_temperature
 
-    def update_temperature(self) -> None:
+    def update_temperature(self) -> bool:
         '''
         Updates temperature as an average of the 20 last thermometer readings
+        
+        return:
+            <bool> 
         '''
-        self.temperature = sum(self.thermometer_history[-20:])/20
-        self.thermometer_history = []
-    
-    def read_thermometer(self) -> None:
+        self.read_thermometer()
+        if len(self.thermometer_history)>19:
+            self.temperature = sum(self.thermometer_history[-20:])/20
+            self.thermometer_history = []
+            return True
+        return False
+
+    def _read_thermometer(self) -> int:
         '''
         Reads thermometer voltage and converts to degrees.
         Adds this value to thermometer history.
@@ -46,15 +53,16 @@ class Kettle:
         conversion_factor = 3.3/65535
         voltage = self.physical_thermometer.read_u16()*conversion_factor
         self.thermometer_history.append(voltage*100)
+        return voltage*100
 
-    def set_power_level(self, power_level) -> None:
+    def set_power_level(self, power_level_percentage) -> None:
         '''
         Sets output pin high or low depending on power level and time
 
         Args:
             power_level <int64> : percentage
         '''
-        if (self.time_0_s % self.delta_t_duty_cycle) < (power_level / (100/self.delta_t_duty_cycle)):
+        if (self.time_0_s % self.delta_t_duty_cycle) < (power_level_percentage / (100/self.delta_t_duty_cycle)):
             self.physical_output_pin.value(1)
         else:
             self.physical_output_pin.value(0)
--- rpi/main.py
+++ rpi/main.py
@@ -1,76 +1,28 @@
 from machine import Pin, PWM
 from time import sleep_us
-# import pid
-
+from pid import PID
+from kettle import Kettle
 import time
 
-class PID:
-    def __init__(self, P=1.0, I=0.0, D=0.0, setpoint=0.0):
-        self.Kp = P
-        self.Ki = I
-        self.Kd = D
-        self.SetPoint = setpoint
-        self.sample_time = 0.0
-        self.current_time = time.ticks_ms()
-        self.last_time = self.current_time
-        self.clear()
-
-    def clear(self):
-        self.proportional = 0.0
-        self.integral = 0.0
-        self.derivative = 0.0
-        self.last_error = 0.0
-        self.output = 0.0
-
-    def update(self, feedback_value):
-        error = self.SetPoint - feedback_value
-        self.current_time = time.ticks_ms()
-        delta_time = self.current_time - self.last_time
-        delta_error = error - self.last_error
-
-        if delta_time >= self.sample_time:
-            self.integral += error * delta_time
-            self.derivative = 0
-
-            if delta_time > 0:
-                self.derivative = delta_error / delta_time
-
-            self.last_time = self.current_time
-            self.last_error = error
-            self.output = (self.Kp * error) + (self.Ki * self.integral) + (self.Kd * self.derivative)
-
-        return self.output
-
-    def set_setpoint(self, setpoint):
-        self.SetPoint = setpoint
-
-    def set_sampletime(self, sample_time):
-        self.sample_time = sample_time
-
-
-
-led = Pin("LED", Pin.OUT)
-led_pwm = PWM(Pin(22))
-
 def init():
-    led.low()
-    led_pwm.freq(8)
-    return 0
-
+    global pid
+    global kettle
+    pid = PID(setpoint=0, sample_time_ms=10000)
+    kettle = Kettle(physical_output_pin='LED')
 
 def loop():
-    DUTY_CYCLE = 0
-    led_pwm.duty_u16(DUTY_CYCLE)
-    pid = PID(1,0,0,50)
-    pid.set_sampletime(0.1)
-    for i in range(100):
-        print(pid.update(i)%65536)
+    pid.set_setpoint(30)
+    while 1:
+        if kettle.update_temperature():
+            pid.update(kettle.temperature)
+            kettle.set_power_level(pid.output)
+   
 
-    return 1
 
 def main():
     init()
     loop()
     return 1
 
-main()
+if __name__ == "__main__":  
+    main()
